13fe8b5f05a3997238802d03f499a57a2b9fed14
Nfongeh Queen <queennfongeh@gmail.com>
2022-11-26 08:15:36 -0500

    completed project

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Post from './components/Post';
+import Posts from './components/Posts';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <Stack.Navigator>
+              <Stack.Screen name="Posts" component={Posts} options={{
+                  title: 'Posts',
+              }} />
+              <Stack.Screen name="Post" component={Post} options={{
+                  title: 'Post',
+              }} />
+          </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
